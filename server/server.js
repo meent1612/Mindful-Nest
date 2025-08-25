96b94575e82e28900581f1580433fe6b55b8a523
TahmidMubashira <tahmidobonti9@gmail.com>
2025-08-26 02:51:31 +0600

    contact page

--- server/server.js
+++ server/server.js
@@ -2,16 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDb from './db/connect.js';
-import studentRoutes from './routes/route.js';
+import journalRoutes from './routes/journalRoutes.js';
+import contactRoutes from './routes/ContactRoutes.js';
 
-// Load environment variables
+
 dotenv.config();
+connectDb();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/student', studentRoutes);
+app.use('/api/journal-entries', journalRoutes);
+app.use('/api/contact', contactRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
