d0f29779175442d6d692bdc713af07325b7670ca
TahmidMubashira <tahmidobonti9@gmail.com>
2025-08-26 03:00:28 +0600

    contact

--- server/server.js
+++ server/server.js
@@ -17,4 +17,4 @@ app.use('/api/journal-entries', journalRoutes);
 app.use('/api/contact', contactRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
