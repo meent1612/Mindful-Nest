ba0d4c6c6b6b66941290cd82cdbae0977f1375a1
meent1612 <sshayma1612@gmail.com>
2025-08-26 22:34:20 +0600

    added journal to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,6 +31,7 @@ const Footer = () => {
               <li><Link to="/health-wellness">Health & Wellness</Link></li>
               <li><Link to="/relationships">Relationships</Link></li>
               <li><Link to="/treatment">Treatment</Link></li>
+              <li><Link to="/mental-health-journal">Mental Health Journal</Link></li>
             </ul>
           </div>
 
