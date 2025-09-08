98435c09725910a5b2fe702fe0f32ac5b5a34d59
TahmidMubashira <tahmidobonti9@gmail.com>
2025-09-09 02:13:46 +0600

    registration authentication

--- server/server.js
+++ server/server.js
@@ -26,8 +26,9 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // Request logging middleware
 app.use((req, res, next) => {
